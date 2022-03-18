7f4d6b81c5e26df77c48a106964b3c0719560ec5
Ryan Weston <ryanlweston@gmail.com>
2022-03-18 16:07:06 +0000

    feat(app): migrate to vuepress 2

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,27 +1,5 @@
-const path = require('path')
-
 module.exports = {
   title: 'CUDOS Docs',
-  configureWebpack: {
-    module: {
-      rules: [
-        {
-          test: /\.nomnoml$/,
-          use: [
-            {
-              loader: 'file-loader',
-              options: {
-                name: 'assets/img/[name].[hash:8].svg'
-              }
-            },
-            {
-              loader: path.resolve('src/.vuepress/loaders/nomnoml')
-            }
-          ]
-        }
-      ]
-    }
-  },
   head: [
     ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }],
     ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' }],
@@ -31,12 +9,24 @@ module.exports = {
     ['meta', { name: 'msapplication-TileColor', content: '#2d89ef' }],
     ['meta', { name: 'theme-color', content: '#00b0ff' }]
   ],
-  plugins: {
-    '@vuepress/pwa': {
-      serviceWorker: false,
-      updatePopup: false
-    }
-  },
+  plugins: [
+    // [
+    //   '@vuepress/pwa', {
+    //     serviceWorker: false,
+    //     updatePopup: false
+    //   }
+    // ],
+    [
+      '@vuepress/plugin-search',
+      {
+        locales: {
+          '/': {
+            placeholder: 'Search',
+          },
+        },
+      },
+    ],
+  ],
   themeConfig: {
     displayAllHeaders: true,
     docsRepo: 'CudoVentures/cudos-network-docs',
@@ -52,62 +42,61 @@ module.exports = {
     repo: 'CudoVentures/',
     sidebar: [
       {
-        title: 'Learn',
+        text: 'Learn',
         collapsible: true,
         children: ['/learn/cudosnetwork.md', '/earn/staking.md', '/learn/validators.md', '/learn/roadmap.md'],
       },
-
       {
-        title: 'Build',
+        text: 'Build',
         collapsible: true,
         children: ['/build/tbcplaceholder.md', '/build/hardware.md'],
       },
       {
-        title: 'Using CUDOS Tokens',
+        text: 'Using CUDOS Tokens',
         collapsible: true,
         children: ['/mainnet/keplr-explorer.md', '/mainnet/main-proposals.md'],
       },
       {
-        title: 'Developer Guides',
+        text: 'Developer Guides',
         collapsible: true,
         children: ['/build/tbcplaceholder.md'],
       },
       {
-        title: 'Operational Tasks & Troubleshooting',
+        text: 'Operational Tasks & Troubleshooting',
         collapsible: true,
         children: ['/build/tbcplaceholder.md'],
       },
       {
-        title: 'Upgrade',
+        text: 'Upgrade',
         collapsible: true,
         children: ['/build/upgrade.md'],
       },
       {
-        title: 'Testnet',
+        text: 'Testnet',
         collapsible: true,
         children: ['/testnet/testnetoverview.md', '/testnet/testnetenvprep.md', '/testnet/testnetcluster.md', '/testnet/testnetstandalone.md', '/build/fundnodes.md', '/build/securingnodes.md',
           {
-            title: 'Testnet Hardware Requirements',
+            text: 'Testnet Hardware Requirements',
             collapsible: true,
             children: ['/testnet/testnet-hardware.md'],
           },
           {
-            title: 'Using CUDOS Tokens in Testnet',
+            text: 'Using CUDOS Tokens in Testnet',
             collapsible: true,
             children: ['/build/interact-keplr-explorer.md', '/build/proposals.md']
           },
           {
-            title: 'Testnet Operational Tasks and Troubleshooting',
+            text: 'Testnet Operational Tasks and Troubleshooting',
             collapsible: true,
             children: ['/build/validator-restart.md', '/build/sync-troubleshooting.md'],
           },
           {
-            title: 'Testnet Developer Guide',
+            text: 'Testnet Developer Guide',
             collapsible: true,
             children: ['/build/smart-contracts.md', '/build/nft.md'],
           },
           {
-            title: 'Testnet Upgrade',
+            text: 'Testnet Upgrade',
             collapsible: true,
             children: ['/build/upgrade-v0.4']
           },
